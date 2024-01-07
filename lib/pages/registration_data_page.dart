882f63102d043ee8c3d27641a852dd3e3f5d1d73
judenilson <judenilson@hotmail.com>
2024-01-07 15:28:53 -0300

    checkbox

--- lib/pages/registration_data_page.dart
+++ lib/pages/registration_data_page.dart
@@ -1,3 +1,4 @@
+import 'package:dio_flutter_navigation_app/repositories/airplane_repository.dart';
 import 'package:dio_flutter_navigation_app/repositories/level_repository.dart';
 import 'package:dio_flutter_navigation_app/shared/widgets/input_text_label_widget.dart';
 import 'package:flutter/material.dart';
@@ -14,92 +15,102 @@ class _RegistrationDataPageState extends State<RegistrationDataPage> {
   final _birthDateController = TextEditingController(text: '');
   DateTime? _birthDate;
   var levelRepository = LevelRepository();
+  var airplaneRepository = AirplaneRepository();
+  var airplanes = [];
   var levels = [];
   var levelSelected = '';
+  var airplaneSelected = [];
 
   @override
   void initState() {
     levels = levelRepository.returnLevels();
+    airplanes = airplaneRepository.returnAirplanes();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        // Widget para resolver problema com o teclado quando sobe
-        body: SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints:
-                BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
-            // Fim do BO
-
-            child: Scaffold(
-              appBar: AppBar(
-                backgroundColor: Colors.blue[200],
-                title: const Text('Meus Dados'),
-              ),
-              body: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const InputTextLabelWidget(text: 'Nome'),
-                    TextField(
-                      controller: _nameController,
-                    ),
-                    const InputTextLabelWidget(text: 'Data de Nascimento'),
-                    TextField(
-                      controller: _birthDateController,
-                      readOnly: true,
-                      onTap: () async {
-                        var date = await showDatePicker(
-                          context: context,
-                          initialDate: DateTime(2010, 1, 1),
-                          firstDate: DateTime(1920, 1, 1),
-                          lastDate: DateTime(2010, 12, 31),
-                        );
-                        if (date != null) {
-                          _birthDateController.text = date.toString();
-                          _birthDate = date;
-                        }
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[200],
+        title: const Text('Meus Dados'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        child: ListView(
+          // O ListView resolve o erro da tela com conteúdo extenso
+          children: [
+            const InputTextLabelWidget(text: 'Nome'),
+            TextField(
+              controller: _nameController,
+            ),
+            const InputTextLabelWidget(text: 'Data de Nascimento'),
+            TextField(
+              controller: _birthDateController,
+              readOnly: true,
+              onTap: () async {
+                var date = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime(2010, 1, 1),
+                  firstDate: DateTime(1920, 1, 1),
+                  lastDate: DateTime(2010, 12, 31),
+                );
+                if (date != null) {
+                  _birthDateController.text = date.toString();
+                  _birthDate = date;
+                }
+              },
+            ),
+            const InputTextLabelWidget(text: 'Nível de Experiência'),
+            Column(
+              children: levels
+                  .map(
+                    (level) => RadioListTile(
+                      dense: true,
+                      title: Text(level),
+                      selected: levelSelected == level,
+                      value: level,
+                      groupValue: levelSelected,
+                      onChanged: (value) {
+                        debugPrint(value.toString());
+                        setState(() {
+                          levelSelected = value;
+                        });
                       },
                     ),
-                    const InputTextLabelWidget(text: 'Nível de Experiência'),
-                    Column(
-                      children: levels
-                          .map(
-                            (level) => RadioListTile(
-                              dense: true,
-                              title: Text(level),
-                              selected: levelSelected == level,
-                              value: level,
-                              groupValue: levelSelected,
-                              onChanged: (value) {
-                                debugPrint(value.toString());
-                                setState(() {
-                                  levelSelected = value;
-                                });
-                              },
-                            ),
-                          )
-                          .toList(),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        debugPrint(_nameController.text);
-                        debugPrint(_birthDate.toString());
+                  )
+                  .toList(),
+            ),
+            const InputTextLabelWidget(text: 'Aeronaves Preferidas'),
+            Column(
+              children: airplanes
+                  .map(
+                    (airplane) => CheckboxListTile(
+                      title: Text(airplane),
+                      value: airplaneSelected.contains(airplane),
+                      onChanged: (bool? value) {
+                        setState(() {
+                          if (value!) {
+                            airplaneSelected.add(airplane);
+                          } else {
+                            airplaneSelected.remove(airplane);
+                          }
+                        });
                       },
-                      child: const Text(
-                        'Salvar',
-                      ),
                     ),
-                  ],
-                ),
+                  )
+                  .toList(),
+            ),
+            TextButton(
+              onPressed: () {
+                debugPrint(_nameController.text);
+                debugPrint(_birthDate.toString());
+              },
+              child: const Text(
+                'Salvar',
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
---
+++ lib/repositories/airplane_repository.dart
@@ -0,0 +1,5 @@
+class AirplaneRepository{
+  List<String> returnAirplanes(){
+    return ['B737', 'B738', 'C182', 'A320', 'C172', 'E190'];
+  }
+}
