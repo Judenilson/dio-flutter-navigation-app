1fb2516901e41b1423be85e0cf3440fe3db42733
judenilson <judenilson@hotmail.com>
2024-01-07 17:46:31 -0300

    modal bottom sheet

--- lib/shared/widgets/custom_drawer_widget.dart
+++ lib/shared/widgets/custom_drawer_widget.dart
@@ -10,24 +10,50 @@ class CustomDrawerWidget extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const UserAccountsDrawerHeader(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: <Color>[
-                  Color.fromARGB(255, 0, 162, 255),
-                  Color.fromARGB(255, 3, 103, 161),
-                ],
+          InkWell(
+            onTap: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext bc) {
+                    return Wrap(
+                      children: [
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          title: const Text('Camera'),
+                          leading: const Icon(Icons.camera),
+                        ),
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          title: const Text('Galeria'),
+                          leading: const Icon(Icons.photo_library),
+                        ),
+                      ],
+                    );
+                  });
+            },
+            child: const UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: <Color>[
+                    Color.fromARGB(255, 0, 162, 255),
+                    Color.fromARGB(255, 3, 103, 161),
+                  ],
+                ),
               ),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage:
+                    NetworkImage('https://judenilson.com.br/img/avatar.jpg'),
+                child: null,
+              ),
+              accountName: Text('Judenilson'),
+              accountEmail: Text('email@email.com'),
             ),
-            currentAccountPicture: CircleAvatar(
-              backgroundImage:
-                  NetworkImage('https://judenilson.com.br/img/avatar.jpg'),
-              child: null,
-            ),
-            accountName: Text('Judenilson'),
-            accountEmail: Text('email@email.com'),
           ),
           InkWell(
             child: Container(
@@ -56,10 +82,83 @@ class CustomDrawerWidget extends StatelessWidget {
                 child: const Row(
                   children: [
                     Icon(Icons.assignment),
-                     Text('Termos de uso e privacidade'),
+                    Text('Termos de uso e privacidade'),
                   ],
                 )),
-            onTap: () {},
+            onTap: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext bc) {
+                    return Container(
+                      padding: const EdgeInsets.all(10),
+                      child: ListView(
+                        children: const [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Termos de uso e privacidade',
+                                textAlign: TextAlign.right,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              Text(
+                                '- Todos os Tripulante devem manter a discrição e a ética no que diz respeito a assuntos, atividades e informações da VASPv.',
+                                textAlign: TextAlign.justify,
+                              ),
+                              Text(
+                                '- O Tripulante é totalmente responsável por suas senhas pessoais de acesso aos sistemas, respondendo direta e pessoalmente por todas as ações que venham a ocorrer em consequência da sua utilização.',
+                                textAlign: TextAlign.justify,
+                              ),
+                              Text(
+                                '- O Tripulante deve utilizar os sistemas corporativos e outros recursos de informática apenas para atividades, sempre respeitando os interesses e valores da Companhia. Não é permitido o uso de sistemas da VASPv para benefício próprio ou de terceiros, nem o uso de informações técnicas sem sua autorização do departamento de Tecnologia.',
+                                textAlign: TextAlign.justify,
+                              ),
+                              Text(
+                                '- Lembre-se: o clima agradável de suas atividades é fundamental. Para sua garantia, não é permitido ofender, expor, desqualificar ou realizar comentários impróprios sobre a Companhia, Tripulante e terceiros, bem como divulgar fotos que afetem a integridade da imagem da VASPv via redes sociais, ferramentas corporativas ou qualquer outro meio de comunicação.',
+                                textAlign: TextAlign.justify,
+                              ),
+                              Text(
+                                'Ações que são expressamente proibidas:',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              Text(
+                                '- Agredir verbalmente qualquer pessoa, seja colega de companhia, subordinado ou tripulante.',
+                                textAlign: TextAlign.start,
+                              ),
+                              Text(
+                                '- Violar a privacidade de outro Tripulante.',
+                                textAlign: TextAlign.justify,
+                              ),
+                              Text(
+                                '- Permitir o acesso de terceiros aos serviços da Companhia sem a devida aprovação.',
+                                textAlign: TextAlign.justify,
+                              ),
+                              Text(
+                                '- Utilizar nossa mídia eletrônica para fazer propaganda de produtos, serviços ou qualquer atividade de interesse pessoal, em benefício próprio, de outras pessoas ou de organizações.',
+                                textAlign: TextAlign.justify,
+                              ),
+                              Text(
+                                '- Enviar ou retransmitir correntes de mensagens e ou assuntos não éticos.',
+                                textAlign: TextAlign.justify,
+                              ),
+                              Text(
+                                '- Utilizar endereço eletrônico e grupos de e-mail da companhia para envio de mensagens polêmicas ou provocações aos Tripulante, subordinados e lideranças.',
+                                textAlign: TextAlign.justify,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+            },
           ),
           const Divider(),
           InkWell(
