33da5ad641618bf2a27f81d4c517fdfd0f7456f3
judenilson <judenilson@hotmail.com>
2024-01-07 16:26:33 -0300

    validating fields

--- lib/pages/registration_data_page.dart
+++ lib/pages/registration_data_page.dart
@@ -17,7 +17,7 @@ class _RegistrationDataPageState extends State<RegistrationDataPage> {
   final _levelRepository = LevelRepository();
   final _airplaneRepository = AirplaneRepository();
   var _airplanes = [];
-  var _airplaneSelected = [];
+  final _airplaneSelected = [];
   var _levels = [];
   var _levelSelected = '';
   double _milesFlying = 200.0;
@@ -72,7 +72,8 @@ class _RegistrationDataPageState extends State<RegistrationDataPage> {
                   lastDate: DateTime(2010, 12, 31),
                 );
                 if (date != null) {
-                  _birthDateController.text = '${date.day.toString()}/${date.month.toString()}/${date.year.toString()}';
+                  _birthDateController.text =
+                      '${date.day.toString()}/${date.month.toString()}/${date.year.toString()}';
                   _birthDate = date;
                 }
               },
@@ -143,11 +144,49 @@ class _RegistrationDataPageState extends State<RegistrationDataPage> {
             ),
             TextButton(
               onPressed: () {
+                if (_nameController.text.trim().length < 3) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text('O nome deve ser preenchido')));
+                  return;
+                }
+                if (_birthDate == null) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text('Data de nascimento inválida')));
+                  return;
+                }
+                if (_levelSelected.trim() == '') {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text('O nível deve ser selecionado')));
+                  return;
+                }
+                if (_airplaneSelected.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text('Deve ser selecionado ao menos um avião')));
+                  return;
+                }
+                if (_expTime == 0) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content:
+                          Text('Deve ter ao menos um ano de experiência')));
+                  return;
+                }
+                if (_milesFlying == 0) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content:
+                          Text('Deve gostar de voar alguma quantidade de milhas')));
+                  return;
+                }
+
+                setState(() {
+                  
+                });
+
                 debugPrint(_nameController.text);
                 debugPrint(_birthDate.toString());
                 debugPrint(_airplaneSelected.toString());
                 debugPrint(_expTime.toString());
                 debugPrint(_milesFlying.toString());
+                debugPrint('Dados salvos com sucesso!');
               },
               child: const Text(
                 'Salvar',
