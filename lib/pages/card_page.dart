b8ae5d586f1f46a1ba53c32d3a37a7f32b9680ec
judenilson <judenilson@hotmail.com>
2024-01-07 21:42:36 -0300

    linear progress indicator and load async data

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,5 +1,6 @@
 import 'package:dio_flutter_navigation_app/model/card_detail_model.dart';
 import 'package:dio_flutter_navigation_app/pages/card_detail_page.dart';
+import 'package:dio_flutter_navigation_app/repositories/card_detail_repository.dart';
 import 'package:flutter/material.dart';
 
 class CardPage extends StatefulWidget {
@@ -10,17 +11,20 @@ class CardPage extends StatefulWidget {
 }
 
 class _CardPageState extends State<CardPage> {
-  var cardDetailModel = CardDetailModel(
-      1,
-      'Judenilson',
-      'https://judenilson.com.br/img/avatar.jpg',
-      'Is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'
-          's standard dummy text ever since the 1500s, when an unknown '
-          'printer took a galley of type and scrambled it to make a type specimen book. It has survived '
-          'not only five centuries, but also the leap into electronic typesetting, remaining essentially '
-          'unchanged. It was popularised in the 1960s with the release of Letraset sheets containing '
-          'Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker '
-          'including versions of Lorem Ipsum.');
+  CardDetailModel? cardDetailModel;
+  CardDetailRepository cardDetailRepository = CardDetailRepository();
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+// Criando um novo método, pois initState não aceita carregamento ASYNC
+  void loadData() async {
+    cardDetailModel = await cardDetailRepository.get();
+    setState(() {}); // Não aceita ASYNC tb.
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,66 +33,68 @@ class _CardPageState extends State<CardPage> {
         Container(
           margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           width: double.infinity,
-          child: InkWell(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          CardDetailPage(cardDetailModel: cardDetailModel)));
-            },
-            child: Card(
-              elevation: 8,
-              shadowColor: Colors.grey,
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Hero(
-                            tag: cardDetailModel.id,
-                            child: CircleAvatar(
-                              backgroundImage:
-                                  NetworkImage(cardDetailModel.url),
-                              child: null,
-                            )),
-                        Expanded(
-                          child: Text(
-                            cardDetailModel.title,
-                            textAlign: TextAlign.end,
-                            style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w700,
+          child: cardDetailModel == null
+              ? const LinearProgressIndicator()
+              : InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CardDetailPage(
+                                cardDetailModel: cardDetailModel!)));
+                  },
+                  child: Card(
+                    elevation: 8,
+                    shadowColor: Colors.grey,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Hero(
+                                  tag: cardDetailModel!.id,
+                                  child: CircleAvatar(
+                                    backgroundImage:
+                                        NetworkImage(cardDetailModel!.url),
+                                    child: null,
+                                  )),
+                              Expanded(
+                                child: Text(
+                                  cardDetailModel!.title,
+                                  textAlign: TextAlign.end,
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            '${cardDetailModel!.text.substring(0, (cardDetailModel!.text.length > 150) ? 150 : cardDetailModel!.text.length)}...',
+                            textAlign: TextAlign.justify,
+                          ),
+                          Container(
+                            width: double.infinity,
+                            alignment: Alignment.centerRight,
+                            child: TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'Ler mais...',
+                                style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    Text(
-                      '${cardDetailModel.text.substring(0, (cardDetailModel.text.length > 150) ? 150 : cardDetailModel.text.length)}...',
-                      textAlign: TextAlign.justify,
-                    ),
-                    Container(
-                      width: double.infinity,
-                      alignment: Alignment.centerRight,
-                      child: TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'Ler mais...',
-                          style: TextStyle(
-                            decoration: TextDecoration.underline,
-                          ),
-                        ),
+                        ],
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-          ),
         ),
       ],
     );
